0cc631c5f940ed7785cc5448f93bf7795f8a8d15
Fisikos72500 <134935120+Fisikos72500@users.noreply.github.com>
2025-05-06 14:38:59 +0200

    Add files via upload

--- TQG_solve_v2_bis.py
+++ TQG_solve_v2_bis.py
@@ -1,7 +1,6 @@
 import os
 import warnings
-import numpy as np
-import seaborn as sns                                         
+import numpy as np                                         
 import matplotlib as mpl
 import scipy.linalg as spl
 import matplotlib.pyplot as plt
@@ -25,7 +24,6 @@ print('-----------------------------------------------------')
 
 
 save_png = False
-partie_pos = True # pour n'affichier que la partie positive des k.c_i
 nb_bins = 50
 figsize_tuple = (15,6.5)
 font_size = 17
@@ -47,7 +45,7 @@ dk = Lk/Nk
 
 beta = 0 #1e-11
 #F1star = 0 #1/Rd**2
-F1star = 4
+F1star = 0
 K2 = (k**2 + F1star)*dy**2
 U0= 1
 
@@ -373,214 +371,8 @@ plt.tight_layout()
 if save_png == True:
 	plt.savefig('im_para/'+name_exp+'/fig1_'+name_exp+choice_plot_name+'.png',dpi=300)
 
-'''
-##################################
-# Plot 2
 
 
-
-# derivative of sigma
-
-dsigma = np.zeros_like(sigma_big_img)
-
-for i in range(len(k)-1):
-	dsigma[i] = (sigma_big_img[i+1] - sigma_big_img[i-1])/(2*dk)
-
-
-
-
-fig, ax = plt.subplots(1, 2, figsize=figsize_tuple)
-
-
-fig.suptitle('Experience : '+name_exp)
-
-
-sns.histplot(sigma_big_img,bins=nb_bins,ax=ax[0],kde=True,stat='percent',color='b')
-ax[0].set_xlabel(r'$\sigma$',size=font_size)
-ax[0].set_ylim(0,100)
-ax[0].set_ylabel('Percent %')
-ax[0].spines[['left','right','top','bottom']].set_linewidth(2)
-ax[0].tick_params(right=True,top=True,direction='in',size=4,width=1)
-ax[0].axhline(0, color='gray', linestyle=':')
-ax[0].axvline(0, color='gray', linestyle=':')
-
-
-
-
-if np.abs(np.max(sigma_big_img)) - np.abs(np.min(sigma_big_img)) < 0:
-	ax[0].set_xlim(-np.abs(np.min(sigma_big_img)), np.abs(np.min(sigma_big_img)))
-else:
-	ax[0].set_xlim(-np.abs(np.max(sigma_big_img)), np.abs(np.max(sigma_big_img)))
-
-
-
-sns.histplot(dsigma,bins=nb_bins,ax=ax[1],kde=True,stat='percent',color='b')
-ax[1].set_xlabel(r'$\partial \sigma/\partial k$',size=font_size)
-
-
-ax[1].set_ylim(0,100)
-
-ax[1].spines[['left','right','top','bottom']].set_linewidth(2)
-ax[1].axhline(0, color='gray', linestyle=':')
-ax[1].axvline(0, color='gray', linestyle=':')
-ax[1].tick_params(labelleft=False,right=True,top=True,direction='in',size=4,width=1)
-ax[1].set_ylabel('')
-
-
-
-if np.abs(np.max(dsigma)) - np.abs(np.min(dsigma)) < 0:
-	ax[1].set_xlim(-np.abs(np.min(dsigma)), np.abs(np.min(dsigma)))
-else:
-	ax[1].set_xlim(-np.abs(np.max(dsigma)), np.abs(np.max(dsigma)))
-
-
-
-plt.tight_layout()
-
-if save_png == True:
-	plt.savefig('im_para/'+name_exp+'/fig2_'+name_exp+choice_plot_name+'.png',dpi=300)
-
-
-
-
-##################################
-# Plot 3
-
-fig, (ax) = plt.subplots(2,2,figsize=(15,10))
-
-len_fft = 5000
-
-
-fig.suptitle('Experience : '+name_exp)
-
-
-ax[0,0].axhline(0, color='gray', linestyle=':')
-ax[0,0].axvline(0, color='gray', linestyle=':')
-
-
-if partie_pos==False:
-	ax[0,0].plot(k,sigma_big_img,'b:',alpha=0.15)
-	ax[0,0].scatter(k, sigma_big_img, marker='o', color='b', edgecolor='k', alpha=0.6,s=50, label=r'$\phi$')
-	
-	ax[0,0].plot(k,sigma_big_ree,'r:',alpha=0.15)
-	ax[0,0].scatter(k, sigma_big_ree, marker='o', color='r', edgecolor='k', alpha=0.6,s=50, label=r'$\phi$')
-	
-	if np.abs(np.max(sigma_big_img)) - np.abs(np.min(sigma_big_img)) < 0:
-		ax[0,0].set_ylim(-np.abs(np.min(sigma_big_img)), np.abs(np.min(sigma_big_img)))
-	else:
-		ax[0,0].set_ylim(-np.abs(np.max(sigma_big_img)), np.abs(np.max(sigma_big_img)))
-	
-		
-else:
-	ax[0,0].plot(k[sigma_big_img>=0],sigma_big_img[sigma_big_img>=0],'b:',alpha=0.15)
-	ax[0,0].scatter(k[sigma_big_img>=0], sigma_big_img[sigma_big_img>=0], marker='o', color='b', edgecolor='k', alpha=0.6,s=50, label=r'$\phi$')
-	
-	ax1 = ax[0,0].twinx()
-	
-	ax1.plot(k[sigma_big_img>=0],sigma_big_ree[sigma_big_img>=0],'r:',alpha=0.15)
-	ax1.scatter(k[sigma_big_img>=0], sigma_big_ree[sigma_big_img>=0], marker='o', color='r', edgecolor='k', alpha=0.6,s=50, label=r'$\phi$')
-	ax1.tick_params(right=True,direction='in', size=4, width=1,color='red',labelcolor='red')
-	ax1.set_ylabel(r'$\sigma_\mathbf{Re} = \mathbf{Re}\{c\}.k ~\geq~ 0$',size=font_size,color='red')
-	
-	ax1.spines['right'].set_color('red')                         # spine
-	ax1.yaxis.label.set_color('red')
-	ax1.spines['right'].set_linewidth(2.25)  # Adjust thickness here   
-	
-	ax1.set_ylim(0,np.max(sigma_big_ree[sigma_big_img>=0]))
-	
-	
-	if np.abs(np.max(sigma_big_img)) - np.abs(np.min(sigma_big_img)) < 0:
-		ax[0,0].set_ylim(0, np.abs(np.min(sigma_big_img)))
-	else:
-		ax[0,0].set_ylim(0, np.abs(np.max(sigma_big_img)))
-	
-ax[0,0].axhline(0, color='gray', linestyle=':')
-ax[0,0].axvline(0, color='gray', linestyle=':')
-
-for spine in ax[0,0].spines.values():
-    spine.set_linewidth(2)
-
-
-#ax[0,0].set_xscale('log')
-
-# Common elements
-ax[0,0].set_xlabel(r'$k$',size=font_size)
-ax[0,0].set_ylabel(r'$\sigma_\mathbf{Im} = \mathbf{Im}\{c\}.k ~\geq~ 0$',size=font_size,color='blue')
-ax[0,0].set_title(r'$\sigma_\mathbf{Im}$ and $\sigma_\mathbf{Re}$',size=font_size)
-ax[0,0].tick_params(top=True,right=False,direction='in', size=4, width=1,color='blue',labelcolor='blue')
-ax[0,0].spines['left'].set_color('blue')                         # spine
-ax[0,0].yaxis.label.set_color('blue')
-ax[0,0].spines['left'].set_linewidth(2.25)  # Adjust thickness here      
-ax[0,0].tick_params(axis='x', colors='black', direction='in', size=4, width=1)
-
-ax[0,1].scatter(np.real(omega_big_c),np.imag(omega_big_c),color='b',marker='*',s=50,alpha=0.6,edgecolor='k')
-ax[0,1].set_xlabel(r'$\mathbf{Re}\{\omega\}$',size=font_size)
-ax[0,1].set_ylabel(r'$\mathbf{Im}\{\omega\}$',size=font_size)
-ax[0,1].tick_params(right=True,top=True,direction='in',size=4,width=1)
-ax[0,1].axhline(0, color='gray', linestyle=':')
-ax[0,1].axvline(0, color='gray', linestyle=':')
-ax[0,1].set_title(r'Eigenfrequencies $\omega = c.k$')
-# Make the axes (spines) bold
-for spine in ax[0,1].spines.values():
-    spine.set_linewidth(2)
-
-
-
-
-
-# pour k,sigma_big_img
-Nfourier = len(sigma_big_img)
-kmax = k[-1] - k[0]
-Fs = Nfourier/kmax
-fourier11 = np.fft.fft(sigma_big_img,len_fft)/(Nfourier/2)
-freq11 = np.fft.fftfreq(Nfourier,1/Fs)
-
-
-ax[1,0].plot(freq11[0:Nfourier//2],np.abs(fourier11[0:Nfourier//2]),color='r',label=r'DFT of $\sigma$')
-ax[1,0].tick_params(right=True,top=True,direction='in',size=4,width=1)
-ax[1,0].axhline(0, color='gray', linestyle=':')
-ax[1,0].axvline(0, color='gray', linestyle=':')
-ax[1,0].set_xlabel('Frequency',size=font_size)
-ax[1,0].set_ylabel('Amplitude',size=font_size)
-
-ax[1,0].set_ylim(0,np.max(np.abs(fourier11[0:Nfourier//2])))
-ax[1,0].legend(loc='best',fancybox=False,title='Zero-padding : '+str(len_fft-Nfourier))
-
-
-for spine in ax[1,0].spines.values():
-    spine.set_linewidth(2)
-
-
-# pour  np.real(omega_big_c),np.imag(omega_big_c)
-Nfourier = len(omega_big_c)
-x_max = np.real(omega_big_c)[-1] - np.real(omega_big_c)[0]
-Fs = Nfourier/x_max
-fourier12 = np.fft.fft(np.imag(omega_big_c),len_fft)/(Nfourier/2)
-freq12 = np.fft.fftfreq(Nfourier,1/Fs)
-
-
-ax[1,1].plot(freq12[0:Nfourier//2],np.abs(fourier12[0:Nfourier//2]),color='green',label=r'DFT of $\omega$')
-
-ax[1,1].tick_params(labelleft=False,right=True,top=True,direction='in',size=4,width=1)
-ax[1,1].axhline(0, color='gray', linestyle=':')
-ax[1,1].axvline(0, color='gray', linestyle=':')
-ax[1,1].set_xlabel('Frequency',size=font_size)
-	
-ax[1,1].legend(loc='best',fancybox=False,title='Zero-padding : '+str(len_fft-Nfourier))
-ax[1,1].set_ylim(0,np.max(np.abs(fourier11[0:Nfourier//2])))
-
-for spine in ax[1,1].spines.values():
-    spine.set_linewidth(2)
-
-
-plt.tight_layout()
-
-if save_png == True:
-	plt.savefig('im_para/'+name_exp+'/fig3_'+name_exp+choice_plot_name+'.png',dpi=300)
-
-
-
-'''
 plt.show()
 
 print('END')
