d1796ba60a83ea1db1b2fc03e12b5bc434a5d9ee
Fisikos72500 <134935120+Fisikos72500@users.noreply.github.com>
2025-06-03 15:11:16 +0200

    Add files via upload

---
+++ KERNEL_TQG_solve_v2_bis.py
@@ -0,0 +1,275 @@
+import os
+import numpy as np                                        
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from scipy.linalg import eig
+
+mpl.rcParams['font.size'] = 14
+mpl.rcParams['mathtext.fontset'] = 'stix'
+#mpl.rcParams['font.family'] = 'STIXGeneral'
+mpl.rcParams['font.family'] = 'Courier New'
+mpl.rcParams['legend.edgecolor'] = '0'
+
+
+
+
+# cf TQG notes : A.X = c.B.X is the system that is solved here
+# and also the non thermal system
+# @uthor : dimitri moreau 20/05/2025
+
+
+
+
+print('-----------------------------------------------------')
+
+##################################
+# VARIABLES, SPACE ...
+##################################
+
+
+
+
+def compute_sigmas(Ny, Nk, dk, ymin, kmin, Ly, Lk, Lstar, beta, F1star, U0, Theta0_U0, config):
+
+
+	font_size = 17
+
+	choice_plot_name = 'max_sigma_im'
+	
+	Theta0 = Theta0_U0 *U0
+	dy = (Ly - ymin)/Ny
+	y_l, k = np.linspace(ymin,Ly,Ny), np.arange(kmin,Nk*dk,dk)
+	
+	if config == 'conf_1':
+		Un = U0*np.exp(-y_l**2)
+		G12 = -2*y_l*Theta0*np.exp(-y_l**2) # dThetabar/dy
+		Lstar = None
+	elif config == 'conf_2':
+		Un = U0*np.exp(-y_l**2)
+		G12 = -(2/Ly**2)*y_l*Theta0*np.exp(-(y_l**2)/(Lstar**2)) # dThetabar/dy
+	
+	
+	else:
+		import sys
+		sys.exit("ERROR : NO CONFIGURATION")
+		
+	Vn = Un*(dy**2)
+
+
+	G11 = 2.0*Un*(1-2*y_l**2) + F1star*Un + beta - G12
+	F11 = G11*dy**2
+
+	print('/////////////////////////////////////////////////////')
+	print('PARAMS : OK')
+
+
+
+
+	print('/////////////////////////////////////////////////////')
+	print('COMPUTATION...')
+
+	sigma_matrix = np.zeros((len(k),2*Ny))
+	sigmaNT_matrix = np.zeros((len(k),Ny))
+
+	sigma_matrix_ree = np.zeros((len(k),2*Ny))
+	sigmaNT_matrix_ree = np.zeros((len(k),Ny))
+
+	# loop for each case of k
+	for ik in tqdm(range(len(k))):
+
+
+		K2 = (k[ik]**2 + F1star)*dy**2
+
+
+		##################################
+		# CONSTRUCTION OF THE B MATRIX
+		##################################
+
+		
+		B11 = np.zeros((Ny, Ny))
+		
+		
+		for i in range(Ny):
+			B11[i,i] = -(2 + K2)
+			if i>0:
+				B11[i,i-1] = 1.
+			if i<Ny-1:
+				B11[i,i+1] = 1.
+		
+		
+		# Construct other blocks
+		B12 = np.zeros((Ny, Ny))
+		B21 = np.zeros((Ny, Ny))
+		B22 = np.eye(Ny, Ny)
+
+
+		B = np.block([[B11,B12],[B21,B22]])
+
+
+		##################################
+		# CONSTRUCTION OF THE A MATRIX
+		##################################
+
+
+
+		A11 = np.zeros((Ny,Ny))
+		A11_star = np.zeros((Ny,Ny)) # same B11 without the thermal
+		# term that is F11 for the non-TQG solving
+
+		# Block A11
+		
+		for i in range(Ny):
+		
+			A11[i,i] = -Un[i] * (2 + K2) + F11[i]
+			A11_star[i,i] = -Un[i] * (2 + K2) + F11[i] + G12[i]*dy**2
+			if i>0:
+				A11[i,i-1] = Un[i]
+				A11_star[i,i-1] = Un[i]
+			if i<Ny-1:
+				A11[i,i+1] = Un[i]
+				A11_star[i,i+1] = Un[i]
+	    
+
+		
+		# Block A12
+		A12 = np.diag(-Vn)
+		# Block A21
+		A21 = np.diag(G12)
+		# Block A22
+		A22 = np.diag(Un)
+
+		# Final block matrix A
+
+		A = np.block([[A11,A12],[A21,A22]])
+
+
+
+		
+		# velocity odd
+		A[0,1] = 2.0*A[0,1]
+		B[0,1] = 2.0*B[0,1]
+		
+		
+		# velocity not odd
+		#A[0,1]=0.0
+		#B[0,1]=0.0
+
+		A[2*Ny-1,2*Ny-1] = 0.0
+		B[2*Ny-1,2*Ny-1] = 0.0
+		
+		
+		
+		A11[0,1] = 2.0*A11[0,1]
+		A11_star[0,1] = 2.0*A11_star[0,1]
+		B11[0,1] = 2.0*B11[0,1]
+		
+		A11[Ny-1,Ny-1] = 0.0
+		A11_star[Ny-1,Ny-1] = 0.0
+		B11[Ny-1,Ny-1] = 0.0
+		
+		
+
+
+
+
+
+		##################################
+		# SOLUTION
+		##################################
+		# A.X = c.B.X 
+
+
+		###### THERMAL SOLVING (TQG)
+
+		c, X = eig(A,B)
+
+
+
+		sigma = np.imag(c) * k[ik]
+		sigma_matrix[ik,:] = sigma
+		
+
+		
+		sigma_ree = np.real(c) * k[ik]
+		sigma_matrix_ree[ik,:] = sigma_ree
+
+
+
+		###### NON THERMAL SOLVING (QG)
+
+		c_NT, X_NT = eig(A11_star,B11)
+
+		sigma_NT = np.imag(c_NT) * k[ik]
+		sigmaNT_matrix[ik,:] = sigma_NT
+		
+
+		
+		sigma_NT_ree = np.real(c_NT) * k[ik]
+		sigmaNT_matrix_ree[ik,:] = sigma_NT_ree
+
+		
+
+	val_c = np.max(sigma_matrix, axis=1)       
+	val_cNT = np.max(sigmaNT_matrix, axis=1)
+
+	val_c_ree = np.max(sigma_matrix_ree, axis=1)       
+	val_cNT_ree = np.max(sigmaNT_matrix_ree, axis=1)
+	
+	
+	print(sigmaNT_matrix.shape)
+	
+	# Take the maximal value of growth rates for each k
+	max_sigma, max_sigmaNT = np.nanmax(val_c), np.nanmax(val_cNT)
+	
+
+
+	print('COMPUTATION : OK')
+	
+	
+
+
+	##################################
+	# PLOT
+	##################################
+
+
+	print('/////////////////////////////////////////////////////')
+
+	
+	
+
+
+	print('PLOT...')
+
+	fig, (ax) = plt.subplots(1,1)
+
+	ax.plot(k, val_c, 'k--', label='TQG')
+	ax.plot(k, val_cNT, 'k-', label='QG')
+	ax.set_xlabel(r'$k$')
+	ax.set_ylabel(r'$\sigma_\mathbf{Im} = \mathbf{Im}\{c\}.k ~\geq~ 0$')
+	ax.tick_params(top=True,right=True,direction='in',size=4,width=1)
+	ax.legend(fancybox=False)
+	ax.axhline(0, color='gray', linestyle=':')
+	ax.axvline(0, color='gray', linestyle=':')
+	ax.set_ylim(-0.01, 0.5)
+	
+	
+	
+	
+	
+	for spine in ax.spines.values():
+	    spine.set_linewidth(2)
+	
+	
+
+	return Un, G12, fig, (ax), max_sigma, max_sigmaNT 
+	
+	print('END')
+	print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+
+
+
+
---
+++ read_n_go_Theta0_U0.py
@@ -0,0 +1,209 @@
+from KERNEL_TQG_solve_v2_bis import *
+import imageio.v2 as imageio
+import os
+from io import BytesIO
+from PIL import Image
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~~~~~~TQG_SOLVE_2_BIS~~~~~~~~~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('-----------------------------------------------------')
+
+
+#########################
+# To change the variable in the loop
+
+# 1) Change the name of : var_title
+
+# 2) Put a linspace or an arange in your parameter
+#    that you want
+
+# 3) Change the variable after the for var in range ...
+
+# 4) Change the position of the variable inside the output
+#    of the function : compute_sigmas
+
+# 5) Change the name of : save_png (LaTeX format capable)
+
+
+
+
+var_title = 'Theta0_U0'
+config = 'conf_1'
+
+
+
+Ny, Nk = 60, 51
+dk = 0.1
+ymin, kmin, Ly, Lk = 0.1, 0.1, np.pi, 0.1+dk*Nk
+Lstar = 0.1
+
+beta = 0 
+F1star = 0
+U0 = 1
+
+Theta0_U0 = np.round(np.linspace(0., 2., 15), 3)
+
+
+
+# stack maximas into a matrix
+max_sigmas = np.zeros_like(Theta0_U0)
+max_sigmas_NT = np.zeros_like(Theta0_U0)
+
+
+
+# Ensure output dir exists
+os.makedirs('output', exist_ok=True)
+
+# List to store image bytes for the GIF
+frames = []
+
+i = 0
+
+for var in Theta0_U0:
+    Un, G12, fig, (ax), max_sigma, max_sigmaNT = compute_sigmas(Ny, Nk, dk, ymin, kmin, Ly, Lk, Lstar, beta, F1star, U0, var,config)
+    
+    save_png = r'$\Theta_0/U_0 =$'+str(var)
+    ax.set_title(save_png)
+    
+    # Save figure to memory buffer
+    buf = BytesIO()
+    fig.savefig(buf, format='png', dpi=150)
+    buf.seek(0)
+    image = imageio.imread(buf)
+    frames.append(image)
+    buf.close()
+    plt.close(fig)
+    
+    max_sigmas[i] = max_sigma
+    max_sigmas_NT[i] = max_sigmaNT
+    
+    i = i+1
+
+
+
+frames_pil = [Image.fromarray(frame) for frame in frames]
+frames_pil[0].save('output/sigmas_animation_'+var_title+'_'+config+'.gif', save_all=True, append_images=frames_pil[1:], duration=600, loop=0)
+
+
+print('GIF saved')
+
+
+
+
+
+
+
+# Create full file path
+file_path = os.path.join('output/', 'variables_used_'+var_title+'_'+config+'.txt')
+
+# Open a file in write mode
+with open('output/variables_used_'+var_title+'_'+config+'.txt', 'w') as file:
+    file.write('Used variables for : '+var_title+' '+config+'\n')
+    file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    file.write(f"Ny = {Ny}\n")
+    file.write(f"Nk = {Nk}\n")
+    file.write(f"ymin = {ymin}\n")
+    file.write(f"Ly = {Ly}\n")
+    file.write(f"kmin = {kmin}\n")
+    file.write(f"Lk = {Lk}\n")
+    dy = (Ly-ymin)/Ny
+    file.write(f"dy = {dy}\n")
+    file.write(f"dk = {dk}\n")
+    file.write(f"F1star = {F1star}\n")
+    file.write(f"beta = {beta}\n")
+    file.write(f"U0 = {U0}\n")
+    file.write(f"ratio_Theta0_U0 = {Theta0_U0}\n")
+    file.write(f"Lstar = {Lstar}\n")
+    file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+
+
+
+y_l = np.linspace(ymin,Ly,Ny)
+crit, borne =Un*G12, 0.25*(Un*y_l)**2
+
+fig2, (ax2) = plt.subplots(1,2, figsize = (15,6),sharey=True)
+
+ax2[0].plot(Un,y_l,'b')
+ax2[0].tick_params(top=True,right=True,direction='in',size=4,width=1)
+ax2[0].axhline(0, color='gray', linestyle=':')
+ax2[0].axvline(0, color='gray', linestyle=':')
+#ax2.set_ylim(-0.01, 0.5)
+ax2[0].set_title('Velocity profile')
+ax2[0].set_ylabel(r'$y$')
+ax2[0].set_xlabel(r'$\overline{U}_n$')
+	
+	
+for spine in ax2[0].spines.values():
+    spine.set_linewidth(2)
+    
+    
+
+ax2[1].plot(crit,y_l,'k',label=r'$\overline{U}_n.\frac{\mathrm{d}\overline{\Theta}}{\mathrm{d}y}$')
+ax2[1].plot(borne,y_l,'k--',label=r'$\frac{1}{4}.\left(\overline{U}_n.y\right)^2$')
+ax2[1].fill_betweenx(y_l,crit,borne,color='orange',alpha=0.3)
+ax2[1].tick_params(top=True,right=True,direction='in',size=4,width=1)
+
+ax2[1].axhline(0, color='gray', linestyle=':')
+ax2[1].axvline(0, color='gray', linestyle=':')
+ax2[1].set_title('Stability')
+ax2[1].legend(fancybox=False,loc='upper left')
+ax2[1].set_xlabel(r'$\propto~\left(\overline{U}_n.y\right)^2$')
+#ax2.set_ylim(-0.01, 0.5)
+	
+	
+for spine in ax2[1].spines.values():
+    spine.set_linewidth(2)
+
+
+plt.savefig('output/stab_'+var_title+'_'+config+'.png',dpi=300)
+
+
+
+
+# derivative of sigmas_max
+deriv = np.zeros_like(max_sigmas)
+deriv_NT = np.zeros_like(max_sigmas)
+
+dTheta0_U0 = (Theta0_U0[-1] - Theta0_U0[0])/(len(Theta0_U0))
+
+
+
+for i in range(len(deriv)-1):
+	deriv[i] = (max_sigmas[i+1] - max_sigmas[-1])/dTheta0_U0
+	deriv_NT[i] = (max_sigmas_NT[i+1] - max_sigmas_NT[-1])/dTheta0_U0
+
+
+
+
+fig3, (ax3) = plt.subplots(1,1)
+
+ax3.axhline(0, color='gray', linestyle=':')
+ax3.axvline(0, color='gray', linestyle=':')
+
+ax3.plot(Theta0_U0, deriv, 'k--',label='TQG')
+ax3.plot(Theta0_U0, deriv_NT, 'k-',label='QG')
+ax3.tick_params(top=True,right=True,direction='in',size=4,width=1)
+ax3.set_xlabel(r'$\Theta_0/U_0$')
+ax3.legend(fancybox=False,loc='lower right')
+
+ax3.set_ylabel(r'$\mathrm{d}\sigma_\mathbf{max}/\mathrm{d}(\Theta_0/U_0)$')
+
+ax3.set_title('Behaviour of $\sigma_\mathbf{max}$')
+
+
+for spine in ax3.spines.values():
+    spine.set_linewidth(2)
+
+
+plt.tight_layout()
+
+plt.savefig('output/deriv_'+var_title+'_'+config+'.png',dpi=100)
+
+
+
+
+
+print('Variables saved')
+
+
